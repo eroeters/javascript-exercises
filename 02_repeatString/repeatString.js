999915eeae35e953192e7d7a55c2e779a14d1af3
Eric R <erroeters@gmail.com>
2023-11-28 14:47:13 -0500

    Finish 2nd js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+// const repeatString = function (string, num) {
+//   if (num < 0) return "ERROR";
+//   let str = "";
+//   for (let i = 0; i < num; i++) {
+//     str += string;
+//   }
+//   return str;
+// };
 
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
